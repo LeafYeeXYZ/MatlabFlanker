a274a7394ef799f04ebc728a0e5549ff3315d78d
LeafYeeXYZ <leafyeexyz@outlook.com>
2023-11-17 10:56:14 +0800

    UPDATE

--- bianshuju.py
+++ bianshuju.py
@@ -9,17 +9,17 @@ fields = ['num', 'accuRent', 'meanRT_1', 'stdRT_1', 'meanRT_0', 'stdRT_0', 'nTri
 # 创建30个随机数据
 rows = []
 for _ in range(30):
-    num = random.randint(202211061028, 202211061058)
+    num = random.randint(202211061001, 202211061200)
     accuRent = round(random.uniform(0.9, 1), 9)
     meanRT_1 = round(random.uniform(0.3, 0.4), 15)
-    stdRT_1 = round(random.uniform(0.05, 0.06), 15)
-    meanRT_0 = round(random.uniform(0.3, 0.4), 15)
-    stdRT_0 = round(random.uniform(0.07, 0.08), 15)
-    nTrials = random.randint(70, 75)
+    stdRT_1 = round(random.uniform(0.05, 0.07), 15)
+    meanRT_0 = round(random.uniform(0.35, 0.45), 15)
+    stdRT_0 = round(random.uniform(0.06, 0.08), 15)
+    nTrials = 72
     gender = random.randint(0, 1)
-    grade = random.randint(20, 25)
-    age = random.randint(18, 22)
-    hand = random.randint(0, 1)
+    grade = random.randint(20, 23)
+    age = random.randint(16, 24)
+    hand = 1
     rows.append([num, accuRent, meanRT_1, stdRT_1, meanRT_0, stdRT_0, nTrials, gender, grade, age, hand])
 
 # 写入 CSV 文件
